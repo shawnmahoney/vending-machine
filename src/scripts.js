8a2bc6c42d3f94fadc037f6b473896569aa367ea
shawnmahoney <shawn@mahoneydesigns.com>
2017-06-23 03:36:09 -0700

    Updated functions and layout

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,25 @@
 
 
-// Throw and alert when user clicks the ADD A CREDIT button
-document.getElementById("credit-button").addEventListener("click", function(){
-    alert("You added 1 credit. Now go click some candy!");
-});
+// Handle candy removals
+var candy = document.getElementsByClassName("candy");
+
+var snickerGone1 = function() {candy[0].innerHTML = "Snicker 1 Gone!";};
+var snickerGone2 = function() {candy[1].innerHTML = "Snicker 2 Gone!";};
+var snickerGone3 = function() {candy[2].innerHTML = "Snicker 3 Gone!";};
 
 
-// Throw and alert when user clicks a Snickers bar
-/*document.getElementsByClassName("candy").addEventListener("click", function(){
-    alert("Delicious choice!");
-});*/
+candy[0].addEventListener('click', snickerGone1, false);
+candy[1].addEventListener('click', snickerGone2, false);
+candy[2].addEventListener('click', snickerGone3, false);
 
 
-function myFunction() {
-    var x = document.getElementsByClassName("candy");
-    x[0].innerHTML = "Hello World!";
+
+
+// Handle credit value
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 5);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
 }
